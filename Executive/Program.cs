73f49dc0fe14a04e2fbe7456bc50fe1a12f9b298
Ramaswamy Krishnan Chittur <chittur@gmail.com>
2023-09-05 16:33:51 +0530

    Add project files.

---
+++ Executive/Program.cs
@@ -0,0 +1,73 @@
+﻿/******************************************************************************
+ * Filename    = Program.cs
+ *
+ * Author      = Ramaswamy Krishnan-Chittur
+ *
+ * Product     = ReflectionDemo
+ * 
+ * Project     = Executive
+ *
+ * Description = Demonstrates .NET reflection.
+ *****************************************************************************/
+
+using System.Reflection;
+
+namespace Executive
+{
+    /// <summary>
+    /// Class to demonstrate .NET reflection.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Prints the types and their methods in the given assembly.
+        /// </summary>
+        /// <param name="path">Path to the .NET assembly.</param>
+        static void PrintTypes(string path)
+        {
+            Assembly assembly = Assembly.LoadFrom(path);
+            Console.WriteLine($"Assembly: {assembly.FullName}");
+            Console.WriteLine("=========================");
+            Type[] types = assembly.GetTypes();
+            foreach (Type type in types)
+            {
+                Console.WriteLine(type.FullName);
+                MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
+                foreach (MethodInfo method in methods)
+                {
+                    Console.WriteLine($"\t{method.Name}");
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach (ParameterInfo parameter in parameters)
+                    {
+                        Console.WriteLine($"\t\tParameter={parameter.Name?.ToString()}");
+                        Console.WriteLine($"\t\t\tType={parameter.ParameterType.ToString()}");
+                        Console.WriteLine($"\t\t\tPosition={parameter.Position.ToString()}");
+                        Console.WriteLine($"\t\t\tOptional={parameter.IsOptional.ToString()}");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The executive function.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    Console.WriteLine("Usage: Executive.exe <path of .NET binary>");
+                    return;
+                }
+
+                PrintTypes(args[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Overview
+This is a sample project that demonstrates [.NET Reflection](https://learn.microsoft.com/en-us/dotnet/framework/reflection-and-codedom/reflection ".NET Reflection").
+
+# Environment
+The project builds and runs with Visual Studio Community 2022 when the required workloads are installed.
